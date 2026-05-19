042836343a409725f9c532892e6de7eed0fb13a0
CarlosZeyy <carloszeeyy@gmail.com>
2026-05-19 11:36:11 -0300

    feat: update CORS configuration to specify allowed origins

--- backend/src/main/java/dev/carlosmoises/projeto/enferm/config/Cors.java
+++ backend/src/main/java/dev/carlosmoises/projeto/enferm/config/Cors.java
@@ -11,7 +11,7 @@ public class Cors {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.Arrays.asList("*"));
+        configuration.setAllowedOrigins(java.util.Arrays.asList("https://app.enfermex.cloud", "http://ost67padrs3x92f8oyn5q8qc.2.24.109.11.sslip.io"));
         configuration.setAllowedMethods(java.util.Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.Arrays.asList("Authorization", "Content-Type"));
 
